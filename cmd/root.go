18cca9bb00027310eb47f59f1e0d7988975a7155
Steven Linde <stevenlinde@google.com>
2022-11-06 13:46:55 -0800

    initial commit of cobra cmd example rpc server

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// This represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	// TODO: get the name of the CLI command used from cobra
+	Use:   "rpc-cmd-style",
+	Short: "implements a CLI for either a client or server for the greeter service",
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+
+	// TODO do ENV and file config support too
+	RootCmd.PersistentFlags().Int("port", 10001, "this is the service port")
+}
---
+++ cmd/server.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	gs "myapp/greeterserver"
+	pb "myapp/helloservice"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+)
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "minimal grpc client for greeter service",
+	Run:   doServerRun,
+}
+
+func doServerRun(cmd *cobra.Command, args []string) {
+
+	portParam, _ := cmd.Flags().GetInt("port")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", portParam))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	server := gs.GreeterServerInstance()
+
+	pb.RegisterGreeterServer(s, server)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+
+func init() {
+	RootCmd.AddCommand(serverCmd)
+
+}
