52b9353f0ae819f5f31e30c3a304238786923076
Steven Linde <stevenlinde@google.com>
2023-05-29 15:46:07 -0700

    added tests covering the bulk of the client params

--- cmd/client_test.go
+++ cmd/client_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -12,7 +13,35 @@ func Test_ClientCommand(t *testing.T) {
 	serverCmd.SetArgs([]string{portStr})
 	go GenericCommandRunner(t, serverCmd)
 
-	clientCmd := NewClientCmd()
-	clientCmd.SetArgs([]string{portStr})
-	GenericCommandRunner(t, clientCmd, "Hello, world!")
+	// test the port parameter
+	portClientCmd := NewClientCmd()
+	portClientCmd.SetArgs([]string{portStr})
+	GenericCommandRunner(t, portClientCmd, "Hello, world!")
+
+	// TODO test port validation, ie pass in a negative
+
+	// test passing in a host
+	hostClientCmd := NewClientCmd()
+	hostClientCmd.SetArgs([]string{portStr, "--host=127.0.0.1"})
+	GenericCommandRunner(t, hostClientCmd, "Hello, world!")
+
+	// test a specific name
+	nameCommandClient := NewClientCmd()
+	nameStr := "Sunshine"
+	nameCommandClient.SetArgs([]string{portStr, "--name=" + nameStr})
+	GenericCommandRunner(t, nameCommandClient, "Hello, "+nameStr)
+
+	// test the --times parameter
+	timesCommandClient := NewClientCmd()
+	times := 5
+	timesCommandClient.SetArgs([]string{portStr, fmt.Sprintf("--times=%d", times)})
+	validationStr := fmt.Sprintf("%d of %d", times, times)
+	GenericCommandRunner(t, timesCommandClient, validationStr)
+
+	// test the timeout and the wait params
+
+	waitCommandClient := NewClientCmd()
+	waitCommandClient.SetArgs([]string{portStr, "--wait=10", "--timeout=1"})
+	GenericCommandRunner(t, waitCommandClient, "DeadlineExceeded")
+
 }
